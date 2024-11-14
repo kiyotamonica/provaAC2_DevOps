8861ca2dbb8863371d48744fd3417d038b598705
kiyotamonica <monica.kiyota@gmail.com>
2024-11-14 12:57:35 -0300

    Update no endpoint (item G da documentação) :tada:

--- ead-gamificada/src/main/java/com/example/ead_gamificada/EadGamificadaApplication.java
+++ ead-gamificada/src/main/java/com/example/ead_gamificada/EadGamificadaApplication.java
@@ -2,12 +2,24 @@ package com.example.ead_gamificada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+
+@Controller
+
 public class EadGamificadaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EadGamificadaApplication.class, args);
+	}
+	
+	@RequestMapping ("/")
+	@ResponseBody
+	String home() {
+		return "Hello World ! :)";
 	}
 
 }
